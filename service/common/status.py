41605bb9570ebc110148246c3e6bbb496d3dd1a8
momoxironXe <zimarask@gmail.com>
2024-11-17 13:53:17 -0500

    routes

---
+++ service/common/status.py
@@ -0,0 +1,7 @@
+Code	Status	Description
+200	HTTP_200_OK	Success
+201	HTTP_201_CREATED	The requested resource has been created
+204	HTTP_204_NO_CONTENT	There is no further content
+404	HTTP_404_NOT_FOUND	Could not find the resource requested
+405	HTTP_405_METHOD_NOT_ALLOWED	Invalid HTTP method used on an endpoint
+409	HTTP_409_CONFLICT	There is a conflict with your request
---
+++ service/models.py
@@ -0,0 +1,7 @@
+Attribute	Description
+id	The id of the product
+name	The name of the product
+description	The description of the product
+price	The price of the product
+available	True if the product is available, otherwise False
+category	The category under which the product belongs
